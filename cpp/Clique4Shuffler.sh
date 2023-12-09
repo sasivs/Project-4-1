1cf2aa575269491ec33b1f90e54b51574e35d77a
Nithin <nithinkatkam504106@gmail.com>
2023-12-09 15:49:31 +0530

    outfile name change shell script added ReadGplus updated

---
+++ cpp/Clique4Shuffler.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+g++ Clique4Counting.cpp -o Clique4Counting
+# ./Clique4Counting ../python/data/Gplus/edges.csv -1 0.9 5.2 0.1 1 -1 1 20-1 2
+./Clique4Counting ../python/data/IMDB/edges.csv -1 0.9 7.3 0.1 1 -1 1 20-1 2
