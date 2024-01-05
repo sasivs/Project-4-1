514cb41a87798722ab1efaabe14871051dd66260
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2024-01-05 10:11:04 +0530

    shell file with more commands

--- cpp/LocalARR.sh
+++ cpp/LocalARR.sh
@@ -1,15 +1,27 @@
-!/bin/bash -x
+#!/bin/bash -x
 
 g++ ./LocalARR.cpp -o LocalARR.out
 
-./LocalARR.out ../python/data/Gplus/edges.csv 10000 1 3-0 5-1 
-./LocalARR.out ../python/data/Gplus/edges.csv 20000 1 3-0 5-1 
-./LocalARR.out ../python/data/Gplus/edges.csv 30000 1 3-0 5-1 
-./LocalARR.out ../python/data/Gplus/edges.csv 40000 1 3-0 5-1 
-./LocalARR.out ../python/data/Gplus/edges.csv 50000 1 3-0 5-1 
-./LocalARR.out ../python/data/Gplus/edges.csv 60000 1 3-0 5-1 
-./LocalARR.out ../python/data/Gplus/edges.csv 70000 1 3-0 5-1 
-./LocalARR.out ../python/data/Gplus/edges.csv 80000 1 3-0 5-1 
-./LocalARR.out ../python/data/Gplus/edges.csv 90000 1 3-0 5-1 
-./LocalARR.out ../python/data/Gplus/edges.csv 100000 1 3-0 5-1 
-./LocalARR.out ../python/data/Gplus/edges.csv -1 1 3-0 5-1 
+# ./LocalARR.out ../python/data/Gplus/edges.csv 10000 1 3-0 5-1 
+# ./LocalARR.out ../python/data/Gplus/edges.csv 20000 1 3-0 5-1 
+# ./LocalARR.out ../python/data/Gplus/edges.csv 30000 1 3-0 5-1 
+# ./LocalARR.out ../python/data/Gplus/edges.csv 40000 1 3-0 5-1 
+# ./LocalARR.out ../python/data/Gplus/edges.csv 50000 1 3-0 5-1 
+# ./LocalARR.out ../python/data/Gplus/edges.csv 60000 1 3-0 5-1 
+# ./LocalARR.out ../python/data/Gplus/edges.csv 70000 1 3-0 5-1 
+# ./LocalARR.out ../python/data/Gplus/edges.csv 80000 1 3-0 5-1 
+# ./LocalARR.out ../python/data/Gplus/edges.csv 90000 1 3-0 5-1 
+# ./LocalARR.out ../python/data/Gplus/edges.csv 100000 1 3-0 5-1 
+# ./LocalARR.out ../python/data/Gplus/edges.csv -1 1 3-0 5-1 
+
+./LocalARR.out ../python/data/Gplus/edges.csv 10000 1 6-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 20000 1 6-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 30000 1 6-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 40000 1 6-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 50000 1 6-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 60000 1 6-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 70000 1 6-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 80000 1 6-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 90000 1 6-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 100000 1 6-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv -1 1 6-0 5-1 
