65c9b0d7a53dc4c8b05a604a893e3017857f688e
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2024-01-07 21:48:40 +0530

    added IMDB commands

--- cpp/LocalShuffle.sh
+++ cpp/LocalShuffle.sh
@@ -1,5 +1,6 @@
 g++ ./LocalShuffle.cpp -o LocalShuffle.out
 
+
 ./LocalShuffle.out ../python/data/Gplus/edges.csv 10000 1 0.1 0.9 3.22 6 3-0 5-1  
 ./LocalShuffle.out ../python/data/Gplus/edges.csv 20000 1 0.1 0.9 3.84 6 3-0 5-1 
 ./LocalShuffle.out ../python/data/Gplus/edges.csv 30000 1 0.1 0.9 4.22 6 3-0 5-1 
@@ -95,3 +96,91 @@ g++ ./LocalShuffle.cpp -o LocalShuffle.out
 ./LocalShuffle.out ../python/data/Gplus/edges.csv 90000 1 0.1 0.9 5.28 6 3-1 5-1 
 ./LocalShuffle.out ../python/data/Gplus/edges.csv 100000 1 0.1 0.9 5.38 6 3-1 5-1 
 ./LocalShuffle.out ../python/data/Gplus/edges.csv -1 1 0.1 0.9 5.20 8 3-1 5-1 
+
+./LocalShuffle.out ../python/data/IMDB/edges.csv 10000 1 0.1 0.9 3.22 6 3-0 5-1  
+./LocalShuffle.out ../python/data/IMDB/edges.csv 20000 1 0.1 0.9 3.84 6 3-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 30000 1 0.1 0.9 4.22 6 3-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 40000 1 0.1 0.9 4.49 6 3-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 50000 1 0.1 0.9 4.71 6 3-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 60000 1 0.1 0.9 4.88 6 3-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 70000 1 0.1 0.9 5.03 6 3-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 80000 1 0.1 0.9 5.16 6 3-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 90000 1 0.1 0.9 5.28 6 3-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 100000 1 0.1 0.9 5.38 6 3-0 5-1 
+
+./LocalShuffle.out ../python/data/IMDB/edges.csv 10000 1 0.1 0.9 3.22 6 6-0 5-1  
+./LocalShuffle.out ../python/data/IMDB/edges.csv 20000 1 0.1 0.9 3.84 6 6-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 30000 1 0.1 0.9 4.22 6 6-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 40000 1 0.1 0.9 4.49 6 6-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 50000 1 0.1 0.9 4.71 6 6-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 60000 1 0.1 0.9 4.88 6 6-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 70000 1 0.1 0.9 5.03 6 6-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 80000 1 0.1 0.9 5.16 6 6-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 90000 1 0.1 0.9 5.28 6 6-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 100000 1 0.1 0.9 5.38 6 6-0 5-1 
+
+./LocalShuffle.out ../python/data/IMDB/edges.csv 10000 1 0.1 0.9 3.22 6 2-0 5-1  
+./LocalShuffle.out ../python/data/IMDB/edges.csv 20000 1 0.1 0.9 3.84 6 2-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 30000 1 0.1 0.9 4.22 6 2-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 40000 1 0.1 0.9 4.49 6 2-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 50000 1 0.1 0.9 4.71 6 2-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 60000 1 0.1 0.9 4.88 6 2-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 70000 1 0.1 0.9 5.03 6 2-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 80000 1 0.1 0.9 5.16 6 2-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 90000 1 0.1 0.9 5.28 6 2-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 100000 1 0.1 0.9 5.38 6 2-0 5-1 
+
+./LocalShuffle.out ../python/data/IMDB/edges.csv 10000 1 0.1 0.9 3.22 6 2.5-0 5-1  
+./LocalShuffle.out ../python/data/IMDB/edges.csv 20000 1 0.1 0.9 3.84 6 2.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 30000 1 0.1 0.9 4.22 6 2.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 40000 1 0.1 0.9 4.49 6 2.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 50000 1 0.1 0.9 4.71 6 2.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 60000 1 0.1 0.9 4.88 6 2.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 70000 1 0.1 0.9 5.03 6 2.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 80000 1 0.1 0.9 5.16 6 2.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 90000 1 0.1 0.9 5.28 6 2.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 100000 1 0.1 0.9 5.38 6 2.5-0 5-1 
+
+./LocalShuffle.out ../python/data/IMDB/edges.csv 10000 1 0.1 0.9 3.22 6 1.5-0 5-1  
+./LocalShuffle.out ../python/data/IMDB/edges.csv 20000 1 0.1 0.9 3.84 6 1.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 30000 1 0.1 0.9 4.22 6 1.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 40000 1 0.1 0.9 4.49 6 1.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 50000 1 0.1 0.9 4.71 6 1.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 60000 1 0.1 0.9 4.88 6 1.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 70000 1 0.1 0.9 5.03 6 1.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 80000 1 0.1 0.9 5.16 6 1.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 90000 1 0.1 0.9 5.28 6 1.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 100000 1 0.1 0.9 5.38 6 1.5-0 5-1 
+
+./LocalShuffle.out ../python/data/IMDB/edges.csv 10000 1 0.1 0.9 3.22 6 0.5-0 5-1  
+./LocalShuffle.out ../python/data/IMDB/edges.csv 20000 1 0.1 0.9 3.84 6 0.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 30000 1 0.1 0.9 4.22 6 0.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 40000 1 0.1 0.9 4.49 6 0.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 50000 1 0.1 0.9 4.71 6 0.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 60000 1 0.1 0.9 4.88 6 0.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 70000 1 0.1 0.9 5.03 6 0.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 80000 1 0.1 0.9 5.16 6 0.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 90000 1 0.1 0.9 5.28 6 0.5-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 100000 1 0.1 0.9 5.38 6 0.5-0 5-1 
+
+./LocalShuffle.out ../python/data/IMDB/edges.csv 10000 1 0.1 0.9 3.22 6 1-0 5-1  
+./LocalShuffle.out ../python/data/IMDB/edges.csv 20000 1 0.1 0.9 3.84 6 1-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 30000 1 0.1 0.9 4.22 6 1-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 40000 1 0.1 0.9 4.49 6 1-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 50000 1 0.1 0.9 4.71 6 1-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 60000 1 0.1 0.9 4.88 6 1-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 70000 1 0.1 0.9 5.03 6 1-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 80000 1 0.1 0.9 5.16 6 1-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 90000 1 0.1 0.9 5.28 6 1-0 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 100000 1 0.1 0.9 5.38 6 1-0 5-1 
+
+./LocalShuffle.out ../python/data/IMDB/edges.csv 10000 1 0.1 0.9 3.22 6 3-1 5-1  
+./LocalShuffle.out ../python/data/IMDB/edges.csv 20000 1 0.1 0.9 3.84 6 3-1 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 30000 1 0.1 0.9 4.22 6 3-1 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 40000 1 0.1 0.9 4.49 6 3-1 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 50000 1 0.1 0.9 4.71 6 3-1 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 60000 1 0.1 0.9 4.88 6 3-1 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 70000 1 0.1 0.9 5.03 6 3-1 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 80000 1 0.1 0.9 5.16 6 3-1 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 90000 1 0.1 0.9 5.28 6 3-1 5-1 
+./LocalShuffle.out ../python/data/IMDB/edges.csv 100000 1 0.1 0.9 5.38 6 3-1 5-1 
