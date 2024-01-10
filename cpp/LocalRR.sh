484baa816ef2dbfb6d53513cadbf893c0d88391a
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2024-01-10 16:22:43 +0530

    added IMDB ep-0.5

--- cpp/LocalRR.sh
+++ cpp/LocalRR.sh
@@ -34,3 +34,14 @@ g++ ./LocalRR.cpp -o LocalRR.out
 ./LocalRR.out ../python/data/IMDB/edges.csv 8000 1 5-1
 ./LocalRR.out ../python/data/IMDB/edges.csv 9000 1 5-1
 ./LocalRR.out ../python/data/IMDB/edges.csv 10000 1 5-1
+
+./LocalRR.out ../python/data/IMDB/edges.csv 1000 0.5 5-1
+./LocalRR.out ../python/data/IMDB/edges.csv 2000 0.5 5-1
+./LocalRR.out ../python/data/IMDB/edges.csv 3000 0.5 5-1
+./LocalRR.out ../python/data/IMDB/edges.csv 4000 0.5 5-1
+./LocalRR.out ../python/data/IMDB/edges.csv 5000 0.5 5-1
+./LocalRR.out ../python/data/IMDB/edges.csv 6000 0.5 5-1
+./LocalRR.out ../python/data/IMDB/edges.csv 7000 0.5 5-1
+./LocalRR.out ../python/data/IMDB/edges.csv 8000 0.5 5-1
+./LocalRR.out ../python/data/IMDB/edges.csv 9000 0.5 5-1
+./LocalRR.out ../python/data/IMDB/edges.csv 10000 0.5 5-1
