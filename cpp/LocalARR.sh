d31f89e06e5c9bfed19579d3190dae5ddcc9e9f0
Sasi Bhushan VS <sasibhushansaladi@gmail.com>
2024-01-11 10:25:28 +0530

    added new commands

--- cpp/LocalARR.sh
+++ cpp/LocalARR.sh
@@ -2,6 +2,66 @@
 
 g++ ./LocalARR.cpp -o LocalARR.out
 
+./LocalARR.out ../python/data/Gplus/edges.csv 10000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 20000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 30000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 40000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 50000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 60000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 70000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 80000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 90000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 100000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv -1 1 0.5-0 5-1 
+
+./LocalARR.out ../python/data/Gplus/edges.csv 10000 1 1-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 20000 1 1-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 30000 1 1-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 40000 1 1-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 50000 1 1-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 60000 1 1-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 70000 1 1-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 80000 1 1-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 90000 1 1-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 100000 1 1-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv -1 1 1-0 5-1 
+
+./LocalARR.out ../python/data/Gplus/edges.csv 10000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 20000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 30000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 40000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 50000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 60000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 70000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 80000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 90000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 100000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv -1 1 1.5-0 5-1 
+
+./LocalARR.out ../python/data/Gplus/edges.csv 10000 1 2-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 20000 1 2-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 30000 1 2-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 40000 1 2-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 50000 1 2-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 60000 1 2-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 70000 1 2-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 80000 1 2-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 90000 1 2-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 100000 1 2-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv -1 1 2-0 5-1 
+
+./LocalARR.out ../python/data/Gplus/edges.csv 10000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 20000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 30000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 40000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 50000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 60000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 70000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 80000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 90000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv 100000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/Gplus/edges.csv -1 1 2.5-0 5-1 
+
 ./LocalARR.out ../python/data/Gplus/edges.csv 10000 1 3-0 5-1 
 ./LocalARR.out ../python/data/Gplus/edges.csv 20000 1 3-0 5-1 
 ./LocalARR.out ../python/data/Gplus/edges.csv 30000 1 3-0 5-1 
@@ -37,6 +97,66 @@ g++ ./LocalARR.cpp -o LocalARR.out
 ./LocalARR.out ../python/data/Gplus/edges.csv 90000 1 3-1 5-1 
 ./LocalARR.out ../python/data/Gplus/edges.csv 100000 1 3-1 5-1 
 ./LocalARR.out ../python/data/Gplus/edges.csv -1 1 3-1 5-1 
+
+./LocalARR.out ../python/data/IMDB/edges.csv 10000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 20000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 30000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 40000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 50000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 60000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 70000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 80000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 90000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 100000 1 0.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv -1 1 0.5-0 5-1 
+
+./LocalARR.out ../python/data/IMDB/edges.csv 10000 1 1-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 20000 1 1-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 30000 1 1-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 40000 1 1-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 50000 1 1-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 60000 1 1-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 70000 1 1-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 80000 1 1-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 90000 1 1-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 100000 1 1-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv -1 1 1-0 5-1 
+
+./LocalARR.out ../python/data/IMDB/edges.csv 10000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 20000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 30000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 40000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 50000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 60000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 70000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 80000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 90000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 100000 1 1.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv -1 1 1.5-0 5-1 
+
+./LocalARR.out ../python/data/IMDB/edges.csv 10000 1 2-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 20000 1 2-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 30000 1 2-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 40000 1 2-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 50000 1 2-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 60000 1 2-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 70000 1 2-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 80000 1 2-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 90000 1 2-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 100000 1 2-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv -1 1 2-0 5-1 
+
+./LocalARR.out ../python/data/IMDB/edges.csv 10000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 20000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 30000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 40000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 50000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 60000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 70000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 80000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 90000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv 100000 1 2.5-0 5-1 
+./LocalARR.out ../python/data/IMDB/edges.csv -1 1 2.5-0 5-1 
 
 ./LocalARR.out ../python/data/IMDB/edges.csv 10000 1 3-0 5-1 
 ./LocalARR.out ../python/data/IMDB/edges.csv 20000 1 3-0 5-1 
--- cpp/LocalShuffle.sh
+++ cpp/LocalShuffle.sh
@@ -1,5 +1,17 @@
 g++ ./LocalShuffle.cpp -o LocalShuffle.out
 
+#To compare with LocalRR
+./LocalShuffle.out ../python/data/Gplus/edges.csv 1000 1 0.1 0.9 1.67 5 3-0 5-1  
+./LocalShuffle.out ../python/data/Gplus/edges.csv 2000 1 0.1 0.9 2.10 5 3-0 5-1 
+./LocalShuffle.out ../python/data/Gplus/edges.csv 3000 1 0.1 0.9 2.38 5 3-0 5-1 
+./LocalShuffle.out ../python/data/Gplus/edges.csv 4000 1 0.1 0.9 2.60 5 3-0 5-1 
+./LocalShuffle.out ../python/data/Gplus/edges.csv 5000 1 0.1 0.9 2.77 5 3-0 5-1 
+./LocalShuffle.out ../python/data/Gplus/edges.csv 6000 1 0.1 0.9 2.92 5 3-0 5-1 
+./LocalShuffle.out ../python/data/Gplus/edges.csv 7000 1 0.1 0.9 3.05 5 3-0 5-1 
+./LocalShuffle.out ../python/data/Gplus/edges.csv 8000 1 0.1 0.9 3.17 5 3-0 5-1 
+./LocalShuffle.out ../python/data/Gplus/edges.csv 9000 1 0.1 0.9 3.27 5 3-0 5-1 
+./LocalShuffle.out ../python/data/Gplus/edges.csv 10000 1 0.1 0.9 3.36 6 3-0 5-1 
+# ./LocalShuffle.out ../python/data/Gplus/edges.csv -1 1 0.1 0.9 5.20 8 3-0 5-1 
 
 ./LocalShuffle.out ../python/data/Gplus/edges.csv 10000 1 0.1 0.9 3.22 6 3-0 5-1  
 ./LocalShuffle.out ../python/data/Gplus/edges.csv 20000 1 0.1 0.9 3.84 6 3-0 5-1 
